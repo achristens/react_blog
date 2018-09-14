6d704fac512c592bddc8943163862a83da816be2
Abby Christens <achristens@gmail.com>
2018-09-14 15:54:54 -0400

    Adding Switch for react router, adding Link to post Index to add a new post

---
+++ ReduxSimpleStarter/src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        POSTS NEW!
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
